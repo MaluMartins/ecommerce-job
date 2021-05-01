885d5285f6182bb457beb9ceed63a30aa7b2350c
Malu <72713887+MaluMartins@users.noreply.github.com>
2021-05-01 04:31:17 -0300

    last adds

---
+++ js/mudar-foto.js
@@ -0,0 +1,15 @@
+function mudarImg01() {
+    document.getElementById("mudarImg").src="./img/imgs-produto/img-produto1.png"
+}
+
+function mudarVideoYT() {
+    document.getElementById("mudarImg").src="https://www.youtube.com/watch?v=hSHerFjgJ0g"
+}
+
+function mudarImg02() {
+    document.getElementById("mudarImg").src="./img/imgs-produto/img-produto2.png"
+}
+
+function mudarVideo() {
+    document.getElementById("mudarImg").src="./videos/magalu.mp4"
+}
