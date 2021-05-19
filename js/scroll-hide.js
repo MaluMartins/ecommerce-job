5c55bab7a6d0491d48042709d85cc101e13e4af6
Malu <72713887+MaluMartins@users.noreply.github.com>
2021-05-19 12:52:04 -0300

    last adjusts

--- js/scroll-hide.js
+++ js/scroll-hide.js
@@ -4,8 +4,10 @@ var myScrollFunc = function () {
     var y = window.scrollY;
     if (y >= 450) {
         document.getElementById("footerNav").className = 'bottomMenu show'
+        document.getElementById("myBtn").style.bottom = "18%"
     } else {
         document.getElementById("footerNav").className = 'bottomMenu hide'
+        document.getElementById("myBtn").style.bottom = "5%"
     }
 };
 
