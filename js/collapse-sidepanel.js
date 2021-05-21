f793a911da22acbd17c0ec1359ab807e721d30fb
Malu <72713887+MaluMartins@users.noreply.github.com>
2021-05-20 21:02:59 -0300

    last adjusts

--- js/collapse-sidepanel.js
+++ js/collapse-sidepanel.js
@@ -3,11 +3,12 @@
 function openNav0() {
   document.getElementById("mySidepanel0").style.width = "250px";
   document.getElementById("mySidepanel0").style.height = "100%";
-  document.querySelector("div.body").style.position ="fixed";
+  document.querySelector("body").style.position ="fixed";
 }
 
 function closeNav0() {
   document.getElementById("mySidepanel0").style.width = "0";
+  document.querySelector("body").style.position ="relative";
 }
 
 //collapse para sidebar
@@ -15,39 +16,43 @@ function closeNav0() {
 function openNav1() {
     document.getElementById("mySidepanel1").style.width = "250px";
     document.getElementById("mySidepanel1").style.height = "100%";
-    document.querySelector("div.body").style.position ="fixed";
+    document.querySelector("body").style.position ="fixed";
 }
   
   function closeNav1() {
     document.getElementById("mySidepanel1").style.width = "0";
+    document.querySelector("body").style.position ="relative";
 }
 
 function openNav2() {
     document.getElementById("mySidepanel2").style.width = "250px";
     document.getElementById("mySidepanel2").style.height = "100%";
-    document.querySelector("div.body").style.position ="fixed";
+    document.querySelector("body").style.position ="fixed";
 }
   
   function closeNav2() {
     document.getElementById("mySidepanel2").style.width = "0";
+    document.querySelector("body").style.position ="relative";
 }
 
 function openNav3() {
     document.getElementById("mySidepanel3").style.width = "250px";
     document.getElementById("mySidepanel3").style.height = "100%";
-    document.querySelector("div.body").style.position ="fixed";
+    document.querySelector("body").style.position ="fixed";
 }
   
   function closeNav3() {
     document.getElementById("mySidepanel3").style.width = "0";
+    document.querySelector("body").style.position ="relative";
 }
 
 function openNav4() {
   document.getElementById("mySidepanel4").style.width = "250px";
   document.getElementById("mySidepanel4").style.height = "100%";
-  document.querySelector("div.body").style.position ="fixed";
+  document.querySelector("body").style.position ="fixed";
 }
 
 function closeNav4() {
   document.getElementById("mySidepanel4").style.width = "0";
+  document.querySelector("body").style.position ="relative";
 }
