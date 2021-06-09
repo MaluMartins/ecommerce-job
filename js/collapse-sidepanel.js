2bb46840258df5ff35300fed21d554fcf1a8b515
Malu <72713887+MaluMartins@users.noreply.github.com>
2021-06-09 07:29:17 -0300

    last adjusts

--- js/collapse-sidepanel.js
+++ js/collapse-sidepanel.js
@@ -3,12 +3,10 @@
 function openNav0() {
   document.getElementById("mySidepanel0").style.width = "250px";
   document.getElementById("mySidepanel0").style.height = "100%";
-  
 }
 
 function closeNav0() {
   document.getElementById("mySidepanel0").style.width = "0";
-  document.querySelector("body").style.position ="relative";
 }
 
 //collapse para sidebar
@@ -16,29 +14,27 @@ function closeNav0() {
 function openNav1() {
     document.getElementById("mySidepanel1").style.width = "250px";
     document.getElementById("mySidepanel1").style.height = "100%";
-    
+
 }
   
   function closeNav1() {
     document.getElementById("mySidepanel1").style.width = "0";
-    document.querySelector("body").style.position ="relative";
 }
 
 function openNav2() {
     document.getElementById("mySidepanel2").style.width = "250px";
     document.getElementById("mySidepanel2").style.height = "100%";
-    
+
 }
   
   function closeNav2() {
     document.getElementById("mySidepanel2").style.width = "0";
-    document.querySelector("body").style.position ="relative";
 }
 
 function openNav3() {
     document.getElementById("mySidepanel3").style.width = "250px";
     document.getElementById("mySidepanel3").style.height = "100%";
-    
+
 }
   
   function closeNav3() {
@@ -49,10 +45,8 @@ function openNav3() {
 function openNav4() {
   document.getElementById("mySidepanel4").style.width = "250px";
   document.getElementById("mySidepanel4").style.height = "100%";
-  
 }
 
 function closeNav4() {
   document.getElementById("mySidepanel4").style.width = "0";
-  document.querySelector("body").style.position ="relative";
 }
